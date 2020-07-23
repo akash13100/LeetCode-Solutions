45939e27e982555e15875a416bb44f08eaa3e51b
Akash Sehgal <56379566+akash13100@users.noreply.github.com>
2020-07-24 02:16:26 +0530

    Update STOI.cpp

--- Recursion/STOI.cpp
+++ Recursion/STOI.cpp
@@ -1,1 +1,23 @@
-
+int stoiutil(string s,unordered_map<char,int> &m)
+{
+    if(s.size()==0)
+        return 0;
+    if(s.size()==1)
+        return m[s[0]];
+    
+    string subs = s.substr(1);
+    int res = m[s[0]];
+    int p = 10;
+    p = pow(10,(int)s.size());
+    res*=p;
+    return res + stoiutil(subs,m);
+}
+int STOI(string s)
+{
+    unordered_map<char,int> m;
+    for(int i = 0; i<10; i++)
+    {
+        m['i'] = i;
+    }
+    return stoiutil(s,m);
+}
